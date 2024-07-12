0c39fce57d1b6161f0247eb66e25a4fcc89f1dca
slava-moiseev-kramatorsk <94477184+slava-moiseev-kramatorsk@users.noreply.github.com>
2024-07-12 18:43:26 +0300

    Revert "commit for ask in the chat"

--- src/main/java/com/example/demo/repository/BookRepositoryImpl.java
+++ src/main/java/com/example/demo/repository/BookRepositoryImpl.java
@@ -1,48 +1,44 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.Book;
-import jakarta.persistence.EntityManager;
 import java.util.List;
-import java.util.Optional;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.EntityTransaction;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
 public class BookRepositoryImpl implements BookRepository {
-    private final EntityManagerFactory entityManagerFactory;
+    private final SessionFactory sessionFactory;
 
     @Override
     public Book save(Book book) {
-        EntityTransaction transaction = null;
-        try (EntityManager entityManager = entityManagerFactory.createEntityManager()){
-            transaction = entityManager.getTransaction();
-            transaction.begin();
-            entityManager.persist(book);
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(book);
             transaction.commit();
             return book;
-        } catch (RuntimeException e) {
-            if (transaction != null && transaction.isActive()) {
+        } catch (Exception e) {
+            if (transaction != null) {
                 transaction.rollback();
             }
             throw new RuntimeException("Save book to DB failed: " + book, e);
-        }
-    }
-
-    @Override
-    public Optional<Book> findById(Long id) {
-        try (EntityManager entityManager = entityManagerFactory.createEntityManager()) {
-            Book book = entityManager.find(Book.class, id);
-            return Optional.ofNullable(book);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
         }
     }
 
     @Override
     public List<Book> findAll() {
-        try (EntityManager entityManager = entityManagerFactory.createEntityManager()) {
-            return entityManager.createQuery("SELECT b FROM Book", Book.class).getResultList();
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("FROM Book", Book.class).getResultList();
         } catch (Exception e) {
             throw new RuntimeException("Can`t upload all books", e);
         }
