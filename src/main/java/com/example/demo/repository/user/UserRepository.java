576aec932f10e44ba94eb44cb11f1c4305e2f6f1
Slava_Moiseev <qualityengineerinkom@gmail.com>
2024-08-16 17:58:31 +0300

    Finished

--- src/main/java/com/example/demo/repository/user/UserRepository.java
+++ src/main/java/com/example/demo/repository/user/UserRepository.java
@@ -1,9 +1,12 @@
 package com.example.demo.repository.user;
 
 import com.example.demo.model.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 }
