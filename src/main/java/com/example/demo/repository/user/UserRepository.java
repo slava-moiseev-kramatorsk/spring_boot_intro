568ad860399577cf829b334aaf511814b3fa5f36
Slava_Moiseev <qualityengineerinkom@gmail.com>
2024-08-17 11:15:06 +0300

    updated1

--- src/main/java/com/example/demo/repository/user/UserRepository.java
+++ src/main/java/com/example/demo/repository/user/UserRepository.java
@@ -2,7 +2,6 @@ package com.example.demo.repository.user;
 
 import com.example.demo.model.User;
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
--- src/main/java/com/example/demo/security/CustomUserDetailsService.java
+++ src/main/java/com/example/demo/security/CustomUserDetailsService.java
@@ -5,7 +5,6 @@ import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
